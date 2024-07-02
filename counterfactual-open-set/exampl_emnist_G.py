90628d6b9735c0d8884b40c834d1418acbaf25af
elTorado <deanheizmann@gmx.ch>
2024-07-02 19:37:03 +0200

    added no_negatives to suffix creator

--- counterfactual-open-set/exampl_emnist_G.py
+++ counterfactual-open-set/exampl_emnist_G.py
@@ -797,6 +797,8 @@ def evaluate(args):
     test_dataset = Dataset(args, args.dataset_root, which_set="test")
 
     num_classes = len(val_dataset.classes)
+    if args.include_unknown:
+        num_classes -= 1
 
     # create data loaders
     val_loader = torch.utils.data.DataLoader(
